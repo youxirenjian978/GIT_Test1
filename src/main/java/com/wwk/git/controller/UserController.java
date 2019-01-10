09ab8f5456dff7453c6741e32fbb00a076e9cf24
文康 <1851956545@qq.com>
2019-01-10 20:14:14 +0800

    the new peoson

--- src/main/java/com/wwk/git/controller/UserController.java
+++ src/main/java/com/wwk/git/controller/UserController.java
@@ -97,6 +97,7 @@ public class UserController extends BaseController {
     }
 
     public void get(){
+        System.out.println("the new people");
         System.out.println("测试的东西！");
     }
 }
